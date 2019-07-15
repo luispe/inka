9644aaebb84413ff1df01e5c4935fe8e85e09959
Luis Pedro Toloy <luispedrotoloy@gmail.com>
2019-07-15 16:14:35 -0300

    create project and install packages works

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
-const create = require("./lib/create-project");
+const create = require("./lib/create-project/create-project");
 
 clear();
 console.log(colors.cyan(figlet.textSync("Inka", { horizontalLayout: "full" })));
@@ -17,6 +17,31 @@ console.log(colors.cyan(figlet.textSync("Inka", { horizontalLayout: "full" })));
 const run = async () => {
   // Create project
   await create.setupProject();
+  // let dir = files.getRoottDirectoryBase();
+
+  // console.log();
+  // console.log("Initialized a git repository.");
+  // console.log();
+  // console.log(`Success! Created a ${data.name} at ${dir}`);
+  // console.log("Inside that directory, you can run commands:");
+  // console.log();
+  // console.log(
+  //   colors.cyan("   npm start"),
+  //   "\n     Start the development server."
+  // );
+  // console.log();
+  // console.log(
+  //   colors.cyan("   npm build"),
+  //   "\n      Bundles the app into static files for production."
+  // );
+  // console.log();
+  // console.log("We suggest that you begin by typing:");
+  // console.log();
+  // console.log(colors.cyan("   cd "), `${data.name}`);
+  // console.log(colors.cyan("   npm start"));
+  // console.log();
+  // console.log("Happy coding!");
+  // console.log();
 };
 
 run();
