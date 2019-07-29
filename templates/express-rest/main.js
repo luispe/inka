5c99694a13d343a593bfded911ad0a5a0c588d70
Luis Pedro Toloy <luispedrotoloy@gmail.com>
2019-07-29 11:53:10 -0300

    Express add errorHandling middleware

--- templates/express-rest/main.js
+++ templates/express-rest/main.js
@@ -4,12 +4,14 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const helmet = require("helmet");
 const { port } = require("./config");
+const errorHandler = require("./app/middlewares/errorHandler");
 
 // Create Express Application
 const app = express();
 app.use(bodyParser.json()).use(helmet());
 
 require("./api/indexRoutes")(app);
+app.use(errorHandler);
 
 // Start the application
 app.listen(port, () => console.log(chalk.cyan(`App listen on port: ${port}`)));
