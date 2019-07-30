cc887aa362c2254a8ca5a171642a462a4e3b9178
Luis Pedro Toloy <luispedrotoloy@gmail.com>
2019-07-30 14:57:33 -0300

    separate command files

--- src/cli.js
+++ src/cli.js
@@ -1,37 +1,13 @@
 #!/usr/bin/env node
 
 import program from "commander";
-import { createProject } from "./main";
-import { promptForMissingOptions } from "./inquirer-prompt";
+import { generate } from "./commands/generate-resources/generate";
+import { newProject } from "./commands/new-project/new-project";
+import { version } from "./commands/version";
 
-program.version("0.1.3");
-
-program
-  .command("new <name>")
-  .option(
-    "-y, --yes",
-    "Create a new project with the default configuration (framework: expressJs, architecture: REST, git init yes, install packages yes)"
-  )
-  .description("Create a new API project")
-  .action(async function(name, cmd) {
-    let options = {
-      skipPrompts: cmd.yes ? true : false,
-      nameProject: name,
-      version: "0.0.0",
-      description: "",
-      entryPoint: "main.js",
-      keywords: "NodeJs, CLI",
-      author: "LuisPe <https://github.com/LuisPe>",
-      license: "MIT",
-      gitRepository: "",
-      git: false,
-      runInstall: true,
-      framework: "express"
-    };
-
-    let resp = await promptForMissingOptions(options);
-    await createProject(resp);
-  });
+version(program);
+newProject(program);
+generate(program);
 
 export async function cli() {
   program.parse(process.argv);
